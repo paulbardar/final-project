f2274a1c8eebdd7e67313e9f3175510290233e92
paulbardar <q.c.tulip@gmail.com>
2019-11-29 01:17:34 +0200

    Fixed main.js and index.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,67 @@
 ;(function(){
     "use strict";
 
-    function openFunction(elem) {
-        var element = document.getElementById('serviceBlock');
-        element.classList.remove('ba-service-block__view-no');
+        var element1 = document.getElementById('feedBlock'),
+            hideFeedButton = document.getElementById('hideFeed');
+        hideFeedButton.addEventListener("click", openFeedFunc);
+        function openFeedFunc() {
+            if (element1.classList.contains('ba-service-block__view-no')){
+                element1.classList.remove('ba-service-block__view-no');
+            } else {
+                element1.classList.add('ba-service-block__view-no');
+            }
+        }
+
+        var element2 = document.getElementById('settingsBlock'),
+            hideSettingsButton = document.getElementById('hideSettings');
+        hideSettingsButton.addEventListener("click", openSettingsFunc);
+
+        function openSettingsFunc() {
+            if (element2.classList.contains('ba-service-block__view-no')){
+                element2.classList.remove('ba-service-block__view-no');
+            } else {
+                element2.classList.add('ba-service-block__view-no');
+            }
+        }
+
+    var element3 = document.getElementById('teamBlock'),
+        hideTeamButton = document.getElementById('hideTeam');
+    hideTeamButton.addEventListener("click", openTeamFunc);
+
+    function openTeamFunc() {
+        if (element3.classList.contains('ba-service-block__view-no')){
+            element3.classList.remove('ba-service-block__view-no');
+        } else {
+            element3.classList.add('ba-service-block__view-no');
+        }
     }
 
+    var element4 = document.getElementById('inboxBlock'),
+        hideInboxButton = document.getElementById('hideInbox');
+    hideInboxButton.addEventListener("click", openTeanFunc);
+
+    function openTeanFunc() {
+        if (element4.classList.contains('ba-service-block__view-no')){
+            element4.classList.remove('ba-service-block__view-no');
+        } else {
+            element4.classList.add('ba-service-block__view-no');
+        }
+    }
+
+    var element5 = document.getElementById('statisticsBlock'),
+        hideStatButton = document.getElementById('hideStatistics');
+    hideStatButton.addEventListener("click", openStatFunc);
+
+    function openStatFunc() {
+        if (element5.classList.contains('ba-service-block__view-no')){
+            element5.classList.remove('ba-service-block__view-no');
+        } else {
+            element5.classList.add('ba-service-block__view-no');
+        }
+    }
 })();
+
+
+
+
+
