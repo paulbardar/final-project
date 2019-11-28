ee9614a062823bf78919c3f1fe3b3b87166d411b
paulbardar <q.c.tulip@gmail.com>
2019-11-28 21:00:53 +0200

    Made section Services with javasript open and close elements

---
+++ js/main.js
@@ -0,0 +1,9 @@
+;(function(){
+    "use strict";
+
+    function openFunction(elem) {
+        var element = document.getElementById('serviceBlock');
+        element.classList.remove('ba-service-block__view-no');
+    }
+
+})();
